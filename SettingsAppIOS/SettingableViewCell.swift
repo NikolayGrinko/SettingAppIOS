f84df2d4544906449b64c7be6cb13a10b1a31666
grinya37 <112849355+grinya37@users.noreply.github.com>
2023-07-18 01:09:59 +0300

    add_class_SettingableViewCell_add_container

---
+++ SettingsAppIOS/SettingableViewCell.swift
@@ -0,0 +1,47 @@
+//
+//  SettingableViewCell.swift
+//  SettingsAppIOS
+//
+//  Created by Николай Гринько on 18.07.2023.
+//
+
+import UIKit
+
+class SettingableViewCell: UITableViewCell {
+
+   static let identifier = "SettingableViewCell"
+    
+    private let iconContainer: UIView = {
+        let view = UIView()
+        view.clipsToBounds = true
+        view.layer.cornerRadius = 10
+        view.layer.masksToBounds = true
+        return view
+    }()
+    
+    private let iconImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.tintColor = .white
+        imageView.contentMode = .scaleAspectFit
+        return imageView
+    }()
+    private let label: UILabel = {
+        let label = UILabel()
+        label.numberOfLines = 1
+        return label
+    }()
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    override func layoutSubviews() {
+        super.layoutSubviews()
+    }
+    override func prepareForReuse() {
+        super.prepareForReuse()
+    }
+}
